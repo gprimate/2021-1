6bd786a3ef8efdf72bc28343011cb9f762a93250
Guilherme Pereira <gprimate@gmail.com>
2021-06-27 20:13:31 -0300

    Criacao do location e constants

---
+++ Redes/TP1/constants.h
@@ -0,0 +1,18 @@
+#ifndef CONSTANTS
+#define CONSTANTS
+
+#define POWER_OF_2 2
+
+#define ADD "add"
+#define LIST "add"
+#define QUERY "query"
+#define REMOVE "rm"
+
+#define BUFFER_SIZE 500
+
+#define MIN_XY_VALUE 0
+#define MAX_XY_VALUE 9999
+
+#define MAX_NUMBER_OF_LOCATIONS 50
+
+#endif
