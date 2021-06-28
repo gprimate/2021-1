c9953df22e6bf25464c8d445216955447d1afbdb
gabcerqueira <gabusilva@hotmail.com>
2021-06-27 23:03:37 -0300

    labels server

--- Redes/TP1/server.c
+++ Redes/TP1/server.c
@@ -17,53 +17,72 @@ void usage(int argc, char **argv) {
 }
 
 int main(int argc, char **argv) {
+
+    // argumentos invalidos
     if (argc < 3) {
         usage(argc, argv);
     }
 
+
+    // criacao storage
     struct sockaddr_storage storage;
     if (0 != server_sockaddr_init(argv[1], argv[2], &storage)) {
         usage(argc, argv);
     }
 
+
+
+    //socket server
     int s;
     s = socket(storage.ss_family, SOCK_STREAM, 0);
     if (s == -1) {
         logexit("socket");
     }
 
+    // socket options
     int enable = 1;
     if (0 != setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int))) {
         logexit("setsockopt");
     }
 
+
+    //bind de porta para o server
     struct sockaddr *addr = (struct sockaddr *)(&storage);
     if (0 != bind(s, addr, sizeof(storage))) {
         logexit("bind");
     }
 
+
+    // w8 connections
     if (0 != listen(s, 10)) {
         logexit("listen");
     }
 
+    // log espera de conexões
     char addrstr[BUFSZ];
     addrtostr(addr, addrstr, BUFSZ);
     printf("bound to %s, waiting connections\n", addrstr);
 
+
+    //loop server
     while (1) {
+
+        // storage e socket client
         struct sockaddr_storage cstorage;
         struct sockaddr *caddr = (struct sockaddr *)(&cstorage);
         socklen_t caddrlen = sizeof(cstorage);
 
+        //acc connections
         int csock = accept(s, caddr, &caddrlen);
         if (csock == -1) {
             logexit("accept");
         }
-
+        
+        //log conexao cliente
         char caddrstr[BUFSZ];
         addrtostr(caddr, caddrstr, BUFSZ);
         printf("[log] connection from %s\n", caddrstr);
-
+        
         char buf[BUFSZ];
         memset(buf, 0, BUFSZ);
         size_t count = recv(csock, buf, BUFSZ - 1, 0);
